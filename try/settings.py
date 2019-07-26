fa1ca26e9fce3bd37e39b4cf9ee4f012002e9f2f
= <dibyojyoti21century@gmail.com>
2019-07-26 12:45:08 +0000

    some security updates

--- try/settings.py
+++ try/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3k#v!bmc$i2ai34+uosgmv)zagezo4i)+ij2nzp71e5orrttz7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
